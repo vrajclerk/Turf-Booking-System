634084505b15e11791115dc90355185f4842074f
VRAJ_CLERK <vrajclerk04@gmail.com>
2024-02-13 09:49:09 +0530

    initialization

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import NormalHeader from './components/Navbars/NormalHeader'
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+      <h1>Turf Booking System</h1>
+      <NormalHeader />
+        </div>
+        </>
+  )
+}
+
+export default App
